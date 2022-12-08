45812bc78fda18fa61f97846716dfab6dbf40f1e
liusize <895898088@qq.com>
2022-12-08 23:25:02 +0800

    :construction: 暂时完成签到功能

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import cookie from 'cookie'
 import {message} from './message'
-import {checkIn} from './check-in'
+import {checkIn_backup} from './check-in'
 import {request} from './request'
-import {asrsea, getEmoji} from "./utils";
+import {sleep} from '@hudiemon/utils'
 
 const main = async () => {
     if (!process.env.COOKIE) {
         message.error('【cookie】未设置');
         return
     }
-    const {__csrf} = cookie.parse(process.env.COOKIE)
-    request.defaults.params = {csrf_token: __csrf}
-    await checkIn(__csrf)
+    const {__csrf: csrf_token} = cookie.parse(process.env.COOKIE)
+    request.defaults.params = {csrf_token}
+    await checkIn_backup(0, csrf_token)
+    await sleep(1000, 2000)
+    await checkIn_backup(1, csrf_token)
 };
 main().finally(message.finally)
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,3 @@
-import forge from 'node-forge'
 import CryptoJS from 'crypto-js'
 import {Emoji, md, MD_TYPE} from './emoji'
 import random from 'lodash/random'
@@ -21,15 +20,13 @@ function rsaEncrypt(a: string, b: any, c: any) {
     return encryptedString(d, a)
 }
 
-export const asrsea = (d: string, e: any, f: any, g: string) => {
+export const asrsea = (d: { [key: string]: any }, e: string, f: string, g: string) => {
     const i = randomString(16)
-    const u = aesEncrypt(d, g)
-    const encText = aesEncrypt(u, i)
-    const encSecKey = rsaEncrypt(i, "010001", "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7")
-    console.log(encText)
-    console.log(encSecKey)
+    const u = aesEncrypt(JSON.stringify(d), g)
+    const params = aesEncrypt(u, i)
+    const encSecKey = rsaEncrypt(i, e, f)
     return {
-        encText,
+        params,
         encSecKey,
     }
 }
